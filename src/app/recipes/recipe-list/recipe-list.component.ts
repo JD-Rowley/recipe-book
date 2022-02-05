b3b3304b4716cdbd8b8302c0df4ddc01355f86b7
JD Rowley <jdrowley1989@gmail.com>
2022-02-05 11:07:06 -0700

    passed data from recipe components to main component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is simply a test','https://c.pxhere.com/images/da/ca/749301471cd7588b899613e8abf3-1620764.jpg!d')
+    new Recipe('A test recipe', 'This is simply a test','https://c.pxhere.com/images/da/ca/749301471cd7588b899613e8abf3-1620764.jpg!d'),
+    new Recipe('A new test recipe', 'This is simply a test','https://c.pxhere.com/images/da/ca/749301471cd7588b899613e8abf3-1620764.jpg!d')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
