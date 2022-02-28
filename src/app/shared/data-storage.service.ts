db55c8772a17cec5badf2e62e58d1d2cf48c695d
JD Rowley <jdrowley1989@gmail.com>
2022-02-28 13:09:33 -0700

    added auth interceptor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -18,19 +18,15 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        return this.authService.user.pipe(take(1), exhaustMap(user => {
-            return this.http.get<Recipe[]>('https://recipe-book-d0f8b-default-rtdb.firebaseio.com/recipes.json',
-            {
-                params: new HttpParams().set('auth', user.token)
+        return this.http.get<Recipe[]>('https://recipe-book-d0f8b-default-rtdb.firebaseio.com/recipes.json').pipe( 
+            map(recipes => {
+                return recipes.map(recipe => {
+                    return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+                });
+            }),
+            tap(recipes => {
+                this.recipeService.setRecipes(recipes);
             })
-        }), 
-        map(recipes => {
-            return recipes.map(recipe => {
-                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-            });
-        }),
-        tap(recipes => {
-            this.recipeService.setRecipes(recipes);
-        }));
+        );
     }
 }
